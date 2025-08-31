ad013785bdb3dd8ad5cd9f9a9bf0c7d8d66a14ad
Keerthan-Karthikeya-14 <keerthankarthikvaggala@gmail.com>
2025-08-31 22:36:30 +0530

    First commit: add project files

---
+++ Health care/dashboard/logins/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, React is working! 🚀</h1>
+      <p>This text comes from App.jsx</p>
+    </div>
+  )
+}
+
+export default App
---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, React is working! 🚀</h1>
+      <p>This text comes from App.jsx</p>
+    </div>
+  )
+}
+
+export default App
+
